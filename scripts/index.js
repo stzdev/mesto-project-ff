3510992b97dfc61af1fe2d63286581efd5b67fd7
Stepan Z <stepanpm7@gmail.com>
2024-11-01 13:32:03 +0300

    finish

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,12 @@
-const cardTemplate = document.querySelector('#card-template').content.cloneNode(true);
+const cardTemplate = document.querySelector('#card-template').content;
 
 function createCard(item, deleteFunction) {
-
-  const newCard = cardTemplate.cloneNode(true);
-
-  newCard.querySelector('.card__image').src = item.link;
-  newCard.querySelector('.card__image').alt = item.name;
+  const newCard = cardTemplate.querySelector('.card').cloneNode(true);
+  const cardImage = newCard.querySelector('.card__image');
+  cardImage.src = item.link;
+  cardImage.alt = item.name;
   newCard.querySelector('.card__title').textContent = item.name;
-
   newCard.querySelector('.card__delete-button').addEventListener('click', deleteFunction);
-
   return newCard;
 }
 
